64b36d5f1cb11d8694ab86749e6258055e13ca45
Anand Suthar <anand.suthar.cg@gmail.com>
2026-04-28 01:26:04 +0530

    Basic server folder structure create

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
